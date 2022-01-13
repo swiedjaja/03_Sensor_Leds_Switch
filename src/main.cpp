405fe690141e30195c2497e9893f12b0a63f4324
swiedjaja <swiedjaja@gmail.com>
2022-01-13 17:10:24 +0700

    fixing timer1Sec.attach_ms call

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,12 @@ void setup() {
 
   Serial.printf("Board: %s\n", ARDUINO_BOARD);
   Serial.printf("DHT Sensor ready, sampling period: %d ms\n", dht.getMinimumSamplingPeriod());  
-  timer1Sec.attach_ms_scheduled(2*dht.getMinimumSamplingPeriod(), onReadSensors);
+  #if defined(ESP8266)
+    timer1Sec.attach_ms_scheduled(2*dht.getMinimumSamplingPeriod(), onReadSensors);
+  #elif defined (ESP32)
+    timer1Sec.attach_ms(2*dht.getMinimumSamplingPeriod(), onReadSensors);
+  #endif  
+
 }
 
 void loop() {
